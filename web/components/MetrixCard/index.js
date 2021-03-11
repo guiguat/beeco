cfee41bf6fae5a20c369514948641ca2b5ad04d7
iuri-pdista <correaiuri86@gmail.com>
2021-03-11 20:48:07 -0300

    Changed MetrixCard parameter props to a single object

--- web/components/MetrixCard/index.js
+++ web/components/MetrixCard/index.js
@@ -1,12 +1,12 @@
 import styles from '../../styles/MetrixCard.module.css'
 
-export default function MetrixCard({number, sizeUnit, text}){
+export default function MetrixCard({data}){
     return(
         <div className = {styles.metrixCard}>
-            <h1 className = {styles.number}>{number}</h1>
+            <h1 className = {styles.number}>{data.number}</h1>
             <div className={styles.subtitle}>
-                <p className = {styles.sizeUnit}>{sizeUnit}</p>
-                <p className = {styles.text}>{text}</p>
+                <p className = {styles.sizeUnit}>{data.sizeUnit}</p>
+                <p className = {styles.text}>{data.text}</p>
             </div>
         </div>
     );
--- web/pages/index.js
+++ web/pages/index.js
@@ -4,7 +4,9 @@ import Button from '../components/Button'
 export default function Home() {
   return (
     <div>
-      <MetrixCard number="120" sizeUnit="mil" text="Bmdia bom dia Bmdia bom dia Bmdia bom dia Bmdia bom dia"></MetrixCard>
+      {/* <MetrixCard data={number:"120", sizeUnit:"mil", text:"Bmdia bom dia Bmdia bom dia Bmdia bom dia Bmdia bom dia"}></MetrixCard> */}
+
+      <Button isOutline={true} text="Saiba mais "></Button>
     </div>
   )
 }
