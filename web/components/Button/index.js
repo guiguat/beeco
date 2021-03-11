cf47442c5e967d9ada61e15c88bdbaa9007c2eb7
iuri-pdista <correaiuri86@gmail.com>
2021-03-11 14:51:22 -0300

    Finished Button component's css

--- web/components/Button/index.js
+++ web/components/Button/index.js
@@ -1,7 +1,11 @@
-import '../../styles/Button.module.css' 
+import styles from '../../styles/Button.module.css' 
 
 export default function Button({text, isOutline, ...rest}){
     return(
-        <button className={`button ${isOutline?"button-outline":""}`} {...rest}> {text} </button>
+        <button className={ `${styles.button} ${isOutline?styles.buttonOutline: null}`} {...rest}> 
+            <p className={styles.text}>
+                {text} 
+            </p>
+        </button>
     );
 }
