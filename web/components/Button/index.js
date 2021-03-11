446eaa957620f7c9f37157a1baf79ed2f9d3fa43
iuri-pdista <correaiuri86@gmail.com>
2021-03-11 11:12:12 -0300

    Moved Button/index.css to ./styles and renamed it to Button.modules.css

--- web/components/Button/index.js
+++ web/components/Button/index.js
@@ -1,4 +1,5 @@
-import "./index.css"
+import '../../styles/Button.module.css' 
+
 export default function Button({text, isOutline, ...rest}){
     return(
         <button className={`button ${isOutline?"button-outline":""}`} {...rest}> {text} </button>
