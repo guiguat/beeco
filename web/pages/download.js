3d89bebd486f22f5d8c5486c1bd6600974656ac9
migu.zchisff@gmail.com <migu.zchisff@gmail.com>
2021-03-12 11:06:22 -0300

    Responsive layout

--- web/pages/download.js
+++ web/pages/download.js
@@ -5,12 +5,12 @@ import Router from 'next/router'
 export default function Download(){
     return(
         <div className={styles.container}>
-            <div className={styles.div1}>
+            <div className={styles.header}>
                 <img src="/images/logoDownload.svg" alt="beeco"/>
                 <p>Bee.co é a plataforma para os trabalhadores do futuro, entre na nossa colmeia e solucione os problemas mais abelhudos de milhões de usuarios. Se você precisa de uma abelha para te ajudar baixe agora e cadastre sua oferta de trabalho.</p>
                 <Button onClick={()=>{Router.push('/')}} isOutline={false} text="Voltar"></Button>
             </div>
-            <div className={styles.div2}>
+            <div className={styles.downloadsection}>
                 <div className={styles.download}>
                     <p className={styles.title}>Desktop</p>
                     <label className={styles.description}>Crie ofertas de trabalho e procure por trabalhos disponiveis.</label>
