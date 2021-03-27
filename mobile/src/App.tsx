03b32679e409d3ea9574a24165e0e58bf1e4f978
iuri-pdista <correaiuri86@gmail.com>
2021-03-27 17:56:36 -0300

    Add global color and text styling variables

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components'
+
+const colors = {
+  white: "#fafafa",
+  yellow: "#FFE000",
+  lightYellow: "#FFE235",
+  black: "#221D1D",
+  lightBlue: "#06ABEC",
+  blue: "#337AB7",
+  lightgrey: "#6C6C6C",
+  lighterGrey: "#B7B7B7",
+  grey: "#3c3c3c",
+  darkgrey: "#1e2021"
+}
+
+const textStyling = {
+  displayHeavyFont: "normal normal bold 3.6rem/4.8rem 'Saira'",
+  headlineLightFont: "normal normal 400 2.4rem/3.2rem 'Saira'",
+  titleHeavyFont: "normal normal bold 2rem/2.8rem 'Saira'",
+  titleLightFont: "normal normal 400 2rem/2.8rem 'Saira'",
+  subheaderLightFont: "normal normal 400 1.6rem/2.4rem 'Archivo'",
+  subheaderHeavyFont: "normal normal 600 1.6rem/2.4rem 'Archivo'",
+  bodyLightFont: "normal normal 400 1.4rem/2rem 'Archivo'"
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Guatura</Text>
-    </View>
+    <ThemeProvider theme={[colors, textStyling]}>
+      <View>Suco mio pinto</View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
