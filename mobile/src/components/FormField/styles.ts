3fbea65a95440289c9b6fa0b255a6fb687176be5
iuri-pdista <correaiuri86@gmail.com>
2021-03-28 17:48:55 -0300

    Add caption formating

--- mobile/src/components/FormField/styles.ts
+++ mobile/src/components/FormField/styles.ts
@@ -1,10 +1,13 @@
 import styled from "../../styles/styled-components";
 
 export const StyledInput = styled.TextInput`
-    background-color: ${props => props.theme.colors.white};
-    border: 1px solid ${props => props.theme.colors.altGrey};
+    background-color: ${props => props.theme.colors.altGrey};
+    border: 1px solid ${props => props.theme.colors.lighterGrey};
     color: ${props =>  props.theme.colors.lightGrey};
     border-radius: ${props => props.theme.borderRadius};
     padding: 12px;
     margin-top: 8px;
+    font-family: 'Archivo_400Regular';
+    font-size: 12px;
+    line-height: 16px;
 `;
