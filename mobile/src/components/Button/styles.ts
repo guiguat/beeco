444534332601b01abc74c9756011ec6fa5ac0395
guiguat <guaturadzn@gmail.com>
2021-03-28 12:42:01 -0300

    resolver problema de tipagem do styled-components

--- mobile/src/components/Button/styles.ts
+++ mobile/src/components/Button/styles.ts
@@ -3,7 +3,7 @@ import styled from "../../styles/styled-components";
 export const Button = styled.Button<{isOutline: boolean}>`
     border-color: ${ props => props.theme.colors.grey};
     border: ${props => props.isOutline ? 1:0};
-    background-color: ${props => props.isOutline ? props.theme.colors.white : props.theme.yellow}
+    background-color: ${props => props.isOutline ? props.theme.colors.white : props.theme.colors.yellow}
     color: ${props  => props.theme.colors.grey};
 `;
 
--- mobile/src/styles/theme.ts
+++ mobile/src/styles/theme.ts
@@ -1,10 +1,10 @@
 import {DefaultTheme} from 'styled-components';
 
-const themes: DefaultTheme = {
+const themes = {
     borderRadius: "6px",
     colors:{
-        white: "#fafafa",
         yellow: "#FFE000",
+        white: "#fafafa",
         lightYellow: "#FFE235",
         black: "#221D1D",
         lightBlue: "#06ABEC",
@@ -14,6 +14,14 @@ const themes: DefaultTheme = {
         grey: "#3c3c3c",
         darkGrey: "#1e2021",
     }
+    // displayHeavyFont: "normal normal bold 3.6rem/4.8rem 'Saira'",
+    // headlineLightFont: "normal normal 400 2.4rem/3.2rem 'Saira'",
+    // titleHeavyFont: "normal normal bold 2rem/2.8rem 'Saira'",
+    // titleLightFont: "normal normal 400 2rem/2.8rem 'Saira'",
+    // subheaderLightFont: "normal normal 400 1.6rem/2.4rem 'Archivo'",
+    // subheaderHeavyFont: "normal normal 600 1.6rem/2.4rem 'Archivo'",
+    // bodyLightFont: "normal normal 400 1.4rem/2rem 'Archivo'"
 };
+export type MyTheme = typeof themes;
 
-export default themes;
+export default {themes};
