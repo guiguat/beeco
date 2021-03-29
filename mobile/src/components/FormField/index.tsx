2fe46d5d986e105a2156a234baf5c97babf31391
iuri-pdista <correaiuri86@gmail.com>
2021-03-29 09:57:06 -0300

    Add warning when field is empty and required

--- mobile/src/components/FormField/index.tsx
+++ mobile/src/components/FormField/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { TextInputProps } from 'react-native';
 import { SubheaderLight } from '../../styles/fonts';
-import {StyledInput} from './styles';
+import {StyledInput, WarningText} from './styles';
 
-const FormField: React.FC<TextInputProps> = ({children, ...props}) => {
+interface Props extends TextInputProps {
+  isFieldRequired: boolean,
+  isFieldSecure?: boolean
+}
+
+const FormField: React.FC<Props> = ({isFieldRequired, isFieldSecure, children, ...props}) => {
+  const [textValue, setTextValue] = useState("");
+  const [isInputWrong, setIsInputWrong] = useState("");
+  useEffect(() => {
+    if (textValue.trim() === "" && isFieldRequired)
+      setIsInputWrong("*O Campo acima é obrigatório");
+    else 
+      setIsInputWrong("");
+    console.log(isInputWrong);
+  });
   return (
     <>
       <SubheaderLight style={ {color:'grey'} } >
         {children}
       </SubheaderLight>
-      <StyledInput {...props}> </StyledInput>
+      <StyledInput {...props} defaultValue=""
+                  onChangeText = { text => {
+                    setTextValue(text);
+                  }}
+                  secureTextEntry={isFieldSecure??false}> 
+      </StyledInput>
+      <WarningText>
+        {isInputWrong}
+      </WarningText>
     </>
   );
 }
