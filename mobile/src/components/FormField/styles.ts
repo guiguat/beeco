a7e5df9a7fd64ee6d1b453bc78667e4df66c7c48
iuri-pdista <correaiuri86@gmail.com>
2021-03-29 09:56:29 -0300

    Fixed warningText css

--- mobile/src/components/FormField/styles.ts
+++ mobile/src/components/FormField/styles.ts
@@ -6,15 +6,16 @@ export const StyledInput = styled.TextInput`
     color: ${props =>  props.theme.colors.lightGrey};
     border-radius: ${props => props.theme.borderRadius};
     padding: 12px;
-    margin-top: 8px;
+    margin-top: 4px;
     font-family: 'Archivo_400Regular';
     font-size: 12px;
     line-height: 16px;
 `;
 
 export const WarningText = styled.Text`
-    font-size: 6px;
+    font-size: 8px;
     line-height: 8px;
     font-family: 'Archivo_400Regular';
-    color: 'red';
+    color: ${props => props.theme.colors.warningRed};
+    margin-top: 4px;
 `;
