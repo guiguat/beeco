272f0294a71a4cd237ca20087eb7baa2df8627b4
iuri-pdista <correaiuri86@gmail.com>
2021-04-22 02:18:24 -0300

    Created Tag styled component

--- mobile/src/components/TaskCard/styles.ts
+++ mobile/src/components/TaskCard/styles.ts
@@ -10,3 +10,13 @@ export const StyledTaskCard = styled.TouchableOpacity`
     border: 1px solid ${(props) => props.theme.colors.lighterGrey};
     background-color: ${(props) => props.theme.colors.white};
 `;
+
+export const Tag = styled.View`
+    border: 1px solid ${(props) => props.theme.colors.lightOrange};
+    border-radius: 10px;
+    padding: 5px 8px;
+    margin-right: 7px;
+    margin-top: 9px;
+    justify-content: center;
+    align-items: center;
+`;
