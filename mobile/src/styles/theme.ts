cce37d0e21399e71d106c9b7df52377841870f31
iuri-pdista <correaiuri86@gmail.com>
2021-04-22 02:09:24 -0300

    Add lightOrange and darkYellow colors

--- mobile/src/styles/theme.ts
+++ mobile/src/styles/theme.ts
@@ -5,6 +5,7 @@ const themes = {
     yellow: '#FFE000',
     white: '#fafafa',
     lightYellow: '#FFE235',
+    darkYellow: '#CC9F02',
     black: '#221D1D',
     lightBlue: '#06ABEC',
     blue: '#337AB7',
@@ -14,6 +15,7 @@ const themes = {
     lightest: '#FFFFFF',
     darkGrey: '#1e2021',
     warningRed: '#FF3232',
+    lightOrange: '#F3BB1C'
   },
 }
 export type MyTheme = typeof themes
