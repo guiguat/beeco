884bde1b051deb9c6a3b72a4b1387aa7b647f9b8
iuri-pdista <correaiuri86@gmail.com>
2021-04-23 03:28:04 -0300

    Fixed OrderByBtn component type confusion

--- mobile/src/components/OrderByBtn/index.tsx
+++ mobile/src/components/OrderByBtn/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { TouchableOpacityProps } from 'react-native';
 import { StyledButton } from './styles';
 import {SmallHeavy} from '../../styles/fonts';
-import themes from '../../styles/theme'
 
 
-const OrderByBtn: React.FC<TouchableOpacityProps> = ({
-  children, ...props }) => {
+const OrderByBtn: React.FC<TouchableOpacityProps> = ({children, ...props }) => {
   return (
     <StyledButton {...props}>
         <SmallHeavy>{children}</SmallHeavy>
