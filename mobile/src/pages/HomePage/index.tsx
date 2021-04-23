209d50221357ac9b16f333e30090d78127358de5
iuri-pdista <correaiuri86@gmail.com>
2021-04-23 01:56:51 -0300

    Changed component name from HomePageHeader to HomePage

--- mobile/src/pages/HomePage/index.tsx
+++ mobile/src/pages/HomePage/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, FlatList} from 'react-native';
 import {StyledView, PseudoDiv, HexBubble, TextWrapper, Input,  } from './styles';
 import {HeadlineLight, CaptionLight} from '../../styles/fonts'
 import OrderByBtn from '../../components/OrderByBtn';
-import TaskCard from '../../components/TaskCard';
+import TaskCard, {Props} from '../../components/TaskCard';
 
 
-const HomePageHeader: React.FC = () => {
+
+const HomePage: React.FC = () => {
     return(
         <StyledView>
             <HexBubble source={require('../../assets/img/hex_bubble.png')}/>
@@ -22,10 +23,8 @@ const HomePageHeader: React.FC = () => {
                 <OrderByBtn>Menor preço</OrderByBtn>
                 <OrderByBtn>Maior preço</OrderByBtn>
             </TextWrapper>
-            <TaskCard info={{title: "Fazer uma logo para mim", minPrice: 12.98, maxPrice: 13.89, date: "22/01/21", city: "Campinas", description:"Enim cupidatat occaecat ut labore qui id tempor id proident sit amet. Excepteur mollit qui pariatur irure esse enim cillum. Quis aliqua esse aute reprehenderit. Aliqua duis officia labore duis ea.", tags:["Design", "LogoDesign", "Creativity"]}}/>      
-            <TaskCard info={{title: "Fazer uma logo para mim", minPrice: 12.98, maxPrice: 13.89, date: "22/01/21", city: "Campinas", description:"Enim cupidatat occaecat ut labore qui id tempor id proident sit amet. Excepteur mollit qui pariatur irure esse enim cillum. Quis aliqua esse aute reprehenderit. Aliqua duis officia labore duis ea.", tags:["Design", "LogoDesign", "Creativity"]}}/>      
         </StyledView>
     );
 }
 
-export default HomePageHeader;
+export default HomePage;
