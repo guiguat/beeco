e555badd316e2165a357c4fcd20bf5248df33b3c
guiguat <guaturadzn@gmail.com>
2021-04-26 11:46:17 -0300

    colocar bubble

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -14,6 +14,7 @@ import {
 } from '@expo-google-fonts/saira'
 import Signup from './pages/Signup'
 import HomePage from './pages/HomePage'
+import EditProfile from './pages/EditProfile'
 
 export default function App() {
   const [archivoLoaded] = useArchivo({
@@ -30,7 +31,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={themes}>
-      <HomePage />
+      <EditProfile />
     </ThemeProvider>
   )
 }
--- mobile/src/pages/EditProfile/styles.ts
+++ mobile/src/pages/EditProfile/styles.ts
@@ -1,6 +1,6 @@
 import DefaultFormField from '../../components/FormField'
 import styled from '../../styles/styled-components'
-export const Container = styled.SafeAreaView`
+export const Container = styled.ScrollView`
   flex: 1;
   background-color: ${(props) => props.theme.colors.white};
 `
@@ -40,4 +40,13 @@ export const InputProfilePhoto = styled.TouchableOpacity`
 export const StyledBackGroundImage = styled.ImageBackground`
   justify-content: center;
   align-items: center;
+`
+
+export const HexBubble = styled.Image`
+  resize-mode: contain;
+  width: 200px;
+  position: absolute;
+  top: -64px;
+  right: -78px;
+  transform: scaleX(-1) rotate(-90deg);
 `
