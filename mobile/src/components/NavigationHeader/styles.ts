1d7fdbe6ec7c36c8d06e4d60057130ed2d21e37b
iuri-pdista <correaiuri86@gmail.com>
2021-04-26 12:10:56 -0300

    Transformed ProfileHeader into NavigationHeader

---
+++ mobile/src/components/NavigationHeader/styles.ts
@@ -0,0 +1,15 @@
+import styled from '../../styles/styled-components'
+
+export const StyledHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${(props) => props.theme.padding} 0;
+  margin-top: ${(props) => props.theme.padding};
+`
+
+export const Row = styled.View`
+  flex-direction:row;
+  justify-content: flex-start;
+  align-items: center;
+`
