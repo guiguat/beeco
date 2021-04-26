e6d57a96386ec391a0b88869185ebb0cd7e1aea7
iuri-pdista <correaiuri86@gmail.com>
2021-04-26 04:08:05 -0300

    Created EditProfile page, but still need to figure out how to include background image

---
+++ mobile/src/pages/EditProfile/index.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { Input } from '../../components/FormField/styles'
+import {
+  Container,
+  FormView,
+  FormField,
+  FormRow,
+  InputProfilePhoto,
+  Content,
+  CurrentProfilePic
+} from './styles'
+import Button from '../../components/Button'
+import { Feather } from '@expo/vector-icons'
+import themes from '../../styles/theme'
+import AddFotoModal from '../../components/AddFotoModal'
+import { ScrollView } from 'react-native-gesture-handler'
+import ProfileHeader from '../../components/ProfileHeader';
+
+const EditProfile: React.FC = () => {
+  const [modalVisible, setModalVisible] = useState(false)
+  return (
+    <Container>
+      <ProfileHeader isEdit={true}/>
+      <AddFotoModal
+        statusBarTranslucent={true}
+        visible={modalVisible}
+        onResult={() => setModalVisible(!modalVisible)}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+      />
+      <ScrollView>
+        <Content>
+          <FormView>
+            <InputProfilePhoto onPress={() => setModalVisible(true)} style={{opacity: 75}}>
+              <Feather name="plus" size={24} color={themes.colors.grey} />
+            </InputProfilePhoto>
+            <FormField required field="Nome">
+              <Input autoCompleteType="name" />
+            </FormField>
+            <FormField required field="Email">
+              <Input autoCompleteType="email" keyboardType="email-address" />
+            </FormField>
+            <FormRow>
+              <FormField required field="Celular">
+                <Input autoCompleteType="tel" keyboardType="phone-pad" />
+              </FormField>
+              <FormField field="Telefone" style={{ marginLeft: 20 }}>
+                <Input autoCompleteType="tel" keyboardType="phone-pad" />
+              </FormField>
+            </FormRow>
+            <Button style={{ marginBottom: 20 }}>Salvar</Button>
+          </FormView>
+        </Content>
+      </ScrollView>
+    </Container>
+  )
+}
+
+export default EditProfile;
